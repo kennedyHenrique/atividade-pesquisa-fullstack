b6b841d7f3343b4495376150dfc2f81dde7ce40c
kennedy <kennedy.henoliveira@outlook.com>
2026-05-16 13:10:05 -0400

    Angular

---
+++ projetoAngular/src/app/features/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+//export class AppComponent {
+//    title = 'Angular Data Binding Exemplo';
+//}
+
+export class AppComponent {
+  imageUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+}
---
+++ projetoAngular/src/app/features/user-profile/user-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+// user-profile.ts
+@Component({
+    selector: 'user-profile',
+    templateUrl: 'user-profile.html',
+    styleUrl: 'user-profile.css',
+})
+export class UserProfile {
+    // Comportamento do Component é definido aqui
+}
